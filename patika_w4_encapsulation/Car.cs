bd5ac7b63e5ca5933c9b6b62c65554c20701d033
YasarDogann <yasardgn99@gmail.com>
2024-10-17 16:17:05 +0300

    encapsulation

---
+++ patika_w4_encapsulation/Car.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace patika_w4_encapsulation
+{
+    public class Car
+    {
+        //Fields 
+        private string brand;
+        private string model;
+        private string color;
+        private int numberOfDoors;
+
+        // Yapıcı method içine parametre alacak
+        public Car(string brand, string model, string color, int numberOfDoors)
+        {
+            Brand = brand;  //gelen marka değeri özellik olan marka değerine atadık
+            Model = model;  //gelen model değerini özellik olan model değerine atadık
+            Color = color; //gelen renk değerini properties olan renk değerine atadık
+            NumberOfDoors = numberOfDoors; //gelen kapı sayısı değerini properties olan renk değerine atadık
+        }
+
+        // Encapsulation
+        public string Brand
+        {
+            get { return brand; } // Marka değerini almak için (okuma)
+            set { brand = value; } // Dışarıdan yeni bir marka değeri atamak için (yazma)
+        }
+        public string Model
+        {
+            get { return model; } // Model değerini almak için
+            set { model = value; } // Dışarıdan yeni bir marka değeri atamak için 
+        }
+        public string Color
+        {
+            get { return color; }  
+            set { color = value; }
+        }
+
+        public int NumberOfDoors
+        {
+
+            get { return numberOfDoors; } // Kapı sayısı değerini aldık okuma
+
+            set //kapı sayısı değerini atamak için 
+            {
+                if (value == 2 || value == 4) //eğer girilen değer 2 veya 4 ise kapı sayısı olrak atanacak
+                {
+                    numberOfDoors = value;
+                }
+                else //değilse hata yazısı ekranda gösterilecek ve kapı sayısı -1 olarak atanacak
+                {
+                    Console.WriteLine("Hata: Kapı sayısı yalnızca 2 veya 4 olabilir. Kapı sayısı -1 olarak ayarlandı");
+                    numberOfDoors = -1;
+                }
+            }
+        }
+
+        public void Display() //bilgileri gösterme methodu
+        {
+            Console.WriteLine($"Arabanın Markası : {brand}");
+            Console.WriteLine($"Arabanın Modeli  : {model}");
+            Console.WriteLine($"Arabanın Rengi   : {color}");
+            Console.WriteLine($"Arabanın Markası : {numberOfDoors}");
+        }
+
+
+    }
+}
---
+++ patika_w4_encapsulation/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace patika_w4_encapsulation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1. Araba Nesnesi 
+            Car bmw = new Car("BMW", "M5", "Siyah", 2);
+            bmw.Display();
+
+            Console.WriteLine("\r\n----------------------------------------------\r\n");
+            // 2. Araba nesnesi
+            Car honda = new Car("Honda", "Civic", "Gırmızı", 4);
+            honda.Display();
+
+            Console.WriteLine("\r\n----------------------------------------------\r\n");
+
+            //3. Araba nesnesinde kapı sayısı 5 girildiği için hata yazısı çıkacak ve kapı sayısı -1 olacak
+            Car volkswagen = new Car("Volkswagen", "Golf5 - R32", "Sarı", 5);
+            volkswagen.Display();
+        }
+    }
+}
